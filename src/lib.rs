8ee93ad9239989ee8e36327d683a243026e27e8c
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-09-12 13:20:02 -0700

    Rename library

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,4 @@
-extern crate duckdb;
-extern crate duckdb_loadable_macros;
-extern crate libduckdb_sys;
+#![warn(clippy::all, nonstandard_style, rust_2018_idioms)]
 
 use duckdb::{
     core::{DataChunkHandle, Inserter, LogicalTypeHandle, LogicalTypeId},
@@ -43,14 +41,17 @@ impl VTab for HelloVTab {
         })
     }
 
-    fn func(func: &TableFunctionInfo<Self>, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
+    fn func(
+        func: &TableFunctionInfo<Self>,
+        output: &mut DataChunkHandle,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let init_data = func.get_init_data();
         let bind_data = func.get_bind_data();
         if init_data.done.swap(true, Ordering::Relaxed) {
             output.set_len(0);
         } else {
             let vector = output.flat_vector(0);
-            let result = CString::new(format!("Rusty Quack {} 🐥", bind_data.name))?;
+            let result = CString::new(format!("Hello, {}!", bind_data.name))?;
             vector.insert(0, result);
             output.set_len(1);
         }
@@ -62,11 +63,9 @@ impl VTab for HelloVTab {
     }
 }
 
-const EXTENSION_NAME: &str = env!("CARGO_PKG_NAME");
-
-#[duckdb_entrypoint_c_api()]
+#[duckdb_entrypoint_c_api(ext_name = "duckdb_warc")]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>(EXTENSION_NAME)
-        .expect("Failed to register hello table function");
+    con.register_table_function::<HelloVTab>("read_warc")
+        .expect("Failed to register read_warc table function");
     Ok(())
 }
