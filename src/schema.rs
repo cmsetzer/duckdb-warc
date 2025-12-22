460abdf4d53b32bc6c1feccec7f4841e13ec86bd
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-12-22 12:03:11 -0800

    Specify explicit field names in schema

--- src/schema.rs
+++ src/schema.rs
@@ -3,28 +3,105 @@ use warc::WarcHeader;
 
 #[derive(Debug)]
 pub struct Field {
+    pub name: &'static str,
     pub header: WarcHeader,
     pub field_type: LogicalTypeId,
 }
 
 pub static WARC_FIELDS: &[Field] = &[
-    Field { header: WarcHeader::RecordID, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::ContentLength, field_type: LogicalTypeId::Integer },
-    Field { header: WarcHeader::Date, field_type: LogicalTypeId::Timestamp },
-    Field { header: WarcHeader::WarcType, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::ContentType, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::ConcurrentTo, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::BlockDigest, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::PayloadDigest, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::IPAddress, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::RefersTo, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::TargetURI, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::Truncated, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::WarcInfoID, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::Filename, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::Profile, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::IdentifiedPayloadType, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::SegmentNumber, field_type: LogicalTypeId::Integer },
-    Field { header: WarcHeader::SegmentOriginID, field_type: LogicalTypeId::Varchar },
-    Field { header: WarcHeader::SegmentTotalLength, field_type: LogicalTypeId::Integer },
+    Field {
+        name: "record_id",
+        header: WarcHeader::RecordID,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "content_length",
+        header: WarcHeader::ContentLength,
+        field_type: LogicalTypeId::Integer,
+    },
+    Field {
+        name: "date",
+        header: WarcHeader::Date,
+        field_type: LogicalTypeId::Timestamp,
+    },
+    Field {
+        name: "type",
+        header: WarcHeader::WarcType,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "content_type",
+        header: WarcHeader::ContentType,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "concurrent_to",
+        header: WarcHeader::ConcurrentTo,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "block_digest",
+        header: WarcHeader::BlockDigest,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "payload_digest",
+        header: WarcHeader::PayloadDigest,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "ip_address",
+        header: WarcHeader::IPAddress,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "refers_to",
+        header: WarcHeader::RefersTo,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "target_uri",
+        header: WarcHeader::TargetURI,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "truncated",
+        header: WarcHeader::Truncated,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "warcinfo_id",
+        header: WarcHeader::WarcInfoID,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "filename",
+        header: WarcHeader::Filename,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "profile",
+        header: WarcHeader::Profile,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "identified_payload_type",
+        header: WarcHeader::IdentifiedPayloadType,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "segment_number",
+        header: WarcHeader::SegmentNumber,
+        field_type: LogicalTypeId::Integer,
+    },
+    Field {
+        name: "segment_origin_id",
+        header: WarcHeader::SegmentOriginID,
+        field_type: LogicalTypeId::Varchar,
+    },
+    Field {
+        name: "segment_total_length",
+        header: WarcHeader::SegmentTotalLength,
+        field_type: LogicalTypeId::Integer,
+    },
 ];
