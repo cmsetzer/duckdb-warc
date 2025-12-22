797a44176c4dd89e74646e80980e78a0ebe9bd2c
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2025-12-22 14:37:08 -0700

    Crude but working implementation

--- src/lib.rs
+++ src/lib.rs
@@ -11,34 +11,48 @@ use duckdb_loadable_macros::duckdb_entrypoint_c_api;
 use libduckdb_sys as ffi;
 use std::{
     error::Error,
-    ffi::CString,
     sync::atomic::{AtomicBool, Ordering},
 };
+use warc::WarcReader;
+
+use crate::schema::WARC_FIELDS;
 
 #[repr(C)]
-struct HelloBindData {
-    name: String,
+struct ReadWarcBindData {
+    filepath: String,
 }
 
 #[repr(C)]
-struct HelloInitData {
+struct ReadWarcInitData {
     done: AtomicBool,
 }
 
-struct HelloVTab;
+struct ReadWarcVTab;
 
-impl VTab for HelloVTab {
-    type InitData = HelloInitData;
-    type BindData = HelloBindData;
+impl VTab for ReadWarcVTab {
+    type InitData = ReadWarcInitData;
+    type BindData = ReadWarcBindData;
 
     fn bind(bind: &BindInfo) -> Result<Self::BindData, Box<dyn std::error::Error>> {
-        bind.add_result_column("column0", LogicalTypeHandle::from(LogicalTypeId::Varchar));
-        let name = bind.get_parameter(0).to_string();
-        Ok(HelloBindData { name })
+        WARC_FIELDS.iter().for_each(|field| {
+            let field_type = match &field.field_type {
+                LogicalTypeId::Varchar => LogicalTypeHandle::from(LogicalTypeId::Varchar),
+                LogicalTypeId::Integer => LogicalTypeHandle::from(LogicalTypeId::Integer),
+                LogicalTypeId::Timestamp => LogicalTypeHandle::from(LogicalTypeId::Timestamp),
+                _ => LogicalTypeHandle::from(LogicalTypeId::Varchar),
+            };
+            bind.add_result_column(field.name, field_type)
+        });
+        bind.add_result_column("body", LogicalTypeHandle::from(LogicalTypeId::Blob));
+
+        let filepath = bind.get_parameter(0).to_string();
+        Ok(ReadWarcBindData { filepath })
     }
 
     fn init(_: &InitInfo) -> Result<Self::InitData, Box<dyn std::error::Error>> {
-        Ok(HelloInitData { done: AtomicBool::new(false) })
+        Ok(ReadWarcInitData {
+            done: AtomicBool::new(false),
+        })
     }
 
     fn func(
@@ -47,14 +61,43 @@ impl VTab for HelloVTab {
     ) -> Result<(), Box<dyn std::error::Error>> {
         let init_data = func.get_init_data();
         let bind_data = func.get_bind_data();
+
         if init_data.done.swap(true, Ordering::Relaxed) {
             output.set_len(0);
         } else {
-            let vector = output.flat_vector(0);
-            let result = CString::new(format!("Hello, {}!", bind_data.name))?;
-            vector.insert(0, result);
-            output.set_len(1);
+            let mut records = Vec::new();
+            let reader = WarcReader::from_path(&bind_data.filepath)?;
+            for record in reader.iter_records() {
+                records.push(record?);
+            }
+
+            for (record_index, record) in records.iter().enumerate() {
+                for (field_index, field) in WARC_FIELDS.iter().enumerate() {
+                    let mut column_vector = output.flat_vector(field_index);
+                    match record.header(field.header.clone()) {
+                        Some(value) => match field.field_type {
+                            LogicalTypeId::Varchar => {
+                                column_vector.insert(record_index, &value.to_string());
+                            }
+                            LogicalTypeId::Integer => {
+                                let slice = column_vector.as_mut_slice::<u32>();
+                                slice[record_index] = value.parse::<u32>()?;
+                            }
+                            _ => {
+                                column_vector.set_null(record_index);
+                            }
+                        },
+                        None => {
+                            column_vector.set_null(record_index);
+                        }
+                    }
+                }
+                let body_vector = output.flat_vector(WARC_FIELDS.len());
+                body_vector.insert(record_index, record.body());
+                output.set_len(records.len());
+            }
         }
+
         Ok(())
     }
 
@@ -65,7 +108,7 @@ impl VTab for HelloVTab {
 
 #[duckdb_entrypoint_c_api(ext_name = "duckdb_warc")]
 pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>("read_warc")
+    con.register_table_function::<ReadWarcVTab>("read_warc")
         .expect("Failed to register read_warc table function");
     Ok(())
 }
